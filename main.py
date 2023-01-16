c7aa3c20e643e184166cae225c62092e94d23952
Alexander Gordey <alexnickgordey3@gmail.com>
2023-01-16 15:24:35 +0300

    еще один коммит

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
+# def plus_perim(func):
+
+#     def wrapper(*args, **kwargs):
+#         return (func(*args, **kwargs))
+#     return wrapper
+#
+#
+# @plus_perim
+# def plosh_tr(a, b, c):
+#     p = (a + b + c) / 2
+#     s =  (p * (p - a) * (p - b) * (p - c)) ** 0.5
+#     return s
+# plosh_tr(5, 7 , 8)
+#
+# print(plus_perim(plus_perim))
 
 
-def is_one_away(word1, word2):
 
-    count = 0
-    for c in word1:
-        if c in word2:
-            count += 1
-    if len(word1) == len(word2) and count == len(word1) - 1:
-        return True
-    else:
-        return False
-w = input()
-w1 =  input()
-print(is_one_away(w,w1))
+
+
+def add_dollar_prefix(func):
+    def wrapper():
+        result = str(func())
+        return '$' + result
+    return wrapper
+
+@add_dollar_prefix
+def get_price(item):
+    prices = {'comic book': 5, 'puzzle': 15}
+    return prices[item]
+
+print(get_price('comic book'))
