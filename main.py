4e6caa1dfbe301bb34a43ea502de51899c04586b
beardedpythonist <alexnickgordey3@gmail.com>
2023-02-09 13:34:50 +0300

    задача про топливо в мейн

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-class Class1:
+def zapravka():
+    distance = 2000
+    consumption = 7.5
+    bak = 70
+    ostatok_na_nachalo = 20
 
-    def init(self, value):
-        self.__var = value
+    dist_na_ostatke = ostatok_na_nachalo * 100 / consumption  # расстояние на остатке
+    print(dist_na_ostatke)
+    spisok_zapravok  = {x: x * 25 for x in range(1, 40)} # заправки: ключ - номер , значение - километраж
+    print(spisok_zapravok)
+    first_pitstop = int
+    ls = []
+    for key, val in spisok_zapravok.items():
+        if dist_na_ostatke <= val:
+            point = key - 1, spisok_zapravok[key-1]  # первая заправка (номер заправки и км, в ввиде кортежа)
+            ls.append(point)
+            print(ls)
+            break
+    dist_na_poln = bak * 100 / consumption  # расстоян на полн баке
+    print(dist_na_poln)
+    dist_to_finish = distance - point
+    
 
-    @property
-    def read_set_del(self):
-        print('Прочтено')
-        return self.__var
-
-    @read_set_del.setter
-    def read_set_del(self, value):
-        self.__var = value
-        print('Изменено')
-    @read_set_del.deleter
-    def read_set_del(self):
-        del self.__var
-        print('Удалено')
-
-c1 = Class1()
-c1.read_set_del = 35
-c1.read_set_del
-del c1.read_set_del
